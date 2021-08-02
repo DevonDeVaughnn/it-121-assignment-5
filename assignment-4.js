9b0cb2de4894c30022607837839864597a18fcda
Darrian DeVaughnn <devondevaughnn@gmail.com>
2021-08-02 16:49:56 -0700

    first commit

---
+++ assignment-4.js
@@ -0,0 +1,17 @@
+let scores = [];
+let scoreEntered;
+let count = 0;
+
+while (scoreEntered != 999) {
+  scoreEntered = prompt("Enter score\nEnter 999 to end entries");
+  scores.push(scoreEntered);
+}
+scores.pop();
+
+for (let i = 0; i < scores.length; i++) {
+  var number = parseInt(scores[i], 10);
+  count += number;
+}
+const average = count / scores.length;
+console.log(count);
+const avg = alert("Average score is " + average);
